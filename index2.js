e9eddd717b59c45edb00bd1447f11db4dc04de4f
Jyotsana0408 <118711814+Jyotsana0408@users.noreply.github.com>
2023-05-10 12:25:02 +0530

    QuerySelector

---
+++ index2.js
@@ -0,0 +1,4 @@
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.visibility = 'hidden';
