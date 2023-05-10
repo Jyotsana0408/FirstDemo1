0dce4c73852b7e635bf38965aa938f9d13c574f1
Jyotsana0408 <118711814+Jyotsana0408@users.noreply.github.com>
2023-05-10 08:52:35 +0530

    HTML and JS added

---
+++ dom.js
@@ -0,0 +1,4 @@
+var headerTitle = document.getElementById('header-title');
+headerTitle.style.borderBottom = 'solid 3px #000';
+document.getElementById("heading").style.fontWeight = "bold";
+document.getElementById("heading").style.color = "green";
